db163eaefeb8a2ac1b01332a85eb9a5a29caa24c
Ruslan Matkovsky <matkovsky.ruslan@gmail.com>
2024-07-23 17:01:40 +0200

    fix middleware

--- src/sxastarter/src/middleware.ts
+++ src/sxastarter/src/middleware.ts
@@ -3,6 +3,9 @@ import { NextRequest, NextResponse } from 'next/server';
 // eslint-disable-next-line
 export default async function (req: NextRequest) {
   const url = req.nextUrl.clone();
+  url.pathname = '/about';
+  url.href = url.href.replace('/post', '/about');
+
   if (url.pathname === '/post') {
     NextResponse.redirect('/about', {
       status: 301,
