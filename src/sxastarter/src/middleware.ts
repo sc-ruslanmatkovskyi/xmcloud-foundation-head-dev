4aa276944d046e992a674d30d6d4325c4951536d
Ruslan Matkovsky <matkovsky.ruslan@gmail.com>
2024-08-02 02:06:13 +0200

    debugger of middleware has been added

--- src/sxastarter/src/middleware.ts
+++ src/sxastarter/src/middleware.ts
@@ -1,10 +1,11 @@
+import { debug } from '@sitecore-jss/sitecore-jss-nextjs/middleware';
 import { NextRequest, NextResponse } from 'next/server';
 
 // eslint-disable-next-line
 export default async function (req: NextRequest) {
   const url = req.nextUrl.clone();
 
-  console.log('REDIRECT MIDDLEWARE');
+  debug.common('REDIRECT MIDDLEWARE');
 
   if (url.pathname.includes('post')) {
     url.pathname = '/about';
