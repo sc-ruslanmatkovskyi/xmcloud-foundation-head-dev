fa090a96e9fde030dc5d084792cafc02833e6688
Ruslan Matkovskyi <ruslan.matkovskyi@sitecore.com>
2024-08-20 08:45:32 +0200

    testing middleware redirect

--- src/sxastarter/src/middleware.ts
+++ src/sxastarter/src/middleware.ts
@@ -1,8 +1,23 @@
-import type { NextRequest, NextFetchEvent } from 'next/server';
+import { type NextRequest, type NextFetchEvent, NextResponse } from 'next/server';
 import middleware from 'lib/middleware';
+import { debug } from '@sitecore-jss/sitecore-jss-nextjs/middleware';
 
 // eslint-disable-next-line
 export default async function (req: NextRequest, ev: NextFetchEvent) {
+  // const existsRedirects = {
+  //   pattern: '/^/test1[/]?$/gi',
+  //   target: 'about',
+  //   redirectType: 'REDIRECT_301',
+  //   isQueryStringPreserved: false,
+  //   locale: '',
+  // };
+  const newUrl = req.nextUrl.clone();
+
+  debug.common('test middleware');
+  const redirectUrl = `${newUrl.origin}/about`;
+  if (newUrl.pathname === '/test1') {
+    return NextResponse.redirect(redirectUrl, 301);
+  }
   return middleware(req, ev);
 }
 
