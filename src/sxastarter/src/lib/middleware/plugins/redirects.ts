5151cdf82d5845abf0612b5c29030377c8c7af5d
Ruslan Matkovskyi <ruslan.matkovskyi@sitecore.com>
2024-09-04 13:59:58 +0200

    added uk-ua

--- src/sxastarter/src/lib/middleware/plugins/redirects.ts
+++ src/sxastarter/src/lib/middleware/plugins/redirects.ts
@@ -14,7 +14,7 @@ class RedirectsPlugin implements MiddlewarePlugin {
       clientFactory,
       // These are all the locales you support in your application.
       // These should match those in your next.config.js (i18n.locales).
-      locales: ['en'],
+      locales: ['en', 'uk-ua'],
       // This function determines if a route should be excluded from RedirectsMiddleware.
       // Certain paths are ignored by default (e.g. Next.js API routes), but you may wish to exclude more.
       // This is an important performance consideration since Next.js Edge middleware runs on every request.
